3b40eee156fb7f0516ded99206d5ca7e8a2fd6b6
SeongMini95 <modify879@gmail.com>
2022-06-09 02:59:58 +0900

    feat: Certify insert 추가

--- mm-api/src/main/java/com/jsm/mm/service/MemberService.java
+++ mm-api/src/main/java/com/jsm/mm/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.jsm.mm.service;
 
+import com.jsm.mm.domain.certify.Certify;
+import com.jsm.mm.domain.certify.enums.CertifyCd;
 import com.jsm.mm.domain.dto.request.member.SignUpRequestDto;
+import com.jsm.mm.domain.member.Member;
 import com.jsm.mm.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final CertifyService certifyService;
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Transactional
     public void signUp(SignUpRequestDto signUpRequestDto) {
         String encPassword = passwordEncoder.encode(signUpRequestDto.getPassword());
-        memberRepository.save(signUpRequestDto.toMemberEntity(encPassword));
+        Member member = memberRepository.save(signUpRequestDto.toMemberEntity(encPassword));
+
+        Certify certify = certifyService.save(member, CertifyCd.SIGN_UP, 30L);
     }
 }
