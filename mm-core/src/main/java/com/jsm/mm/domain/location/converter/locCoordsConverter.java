74730f2abf1ca4fdc002cbcf126c7e8f067fc049
SeongMini95 <modify879@gmail.com>
2022-06-09 20:30:03 +0900

    feat: MultiPolygon converter 수정

--- mm-core/src/main/java/com/jsm/mm/domain/location/converter/locCoordsConverter.java
+++ mm-core/src/main/java/com/jsm/mm/domain/location/converter/locCoordsConverter.java
@@ -3,16 +3,18 @@ package com.jsm.mm.domain.location.converter;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+@Slf4j
 @Converter
 public class locCoordsConverter implements AttributeConverter<MultiPolygon, String> {
 
     @Override
     public String convertToDatabaseColumn(MultiPolygon attribute) {
-        return attribute.toString();
+        return attribute.toText();
     }
 
     @Override
