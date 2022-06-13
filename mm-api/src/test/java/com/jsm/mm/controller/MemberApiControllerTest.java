bf8ab581d05706d70a060dd632d4d010157c80cc
SeongMini95 <modify879@gmail.com>
2022-06-13 19:01:55 +0900

    test: 아이디, 이메일 존재 여부 확인 api / 회원가입에 계정 여부 확인 추가

--- mm-api/src/test/java/com/jsm/mm/controller/MemberApiControllerTest.java
+++ mm-api/src/test/java/com/jsm/mm/controller/MemberApiControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jsm.mm.domain.certify.Certify;
 import com.jsm.mm.domain.certify.repository.CertifyRepository;
 import com.jsm.mm.domain.member.Member;
+import com.jsm.mm.domain.member.enums.RoleName;
 import com.jsm.mm.domain.member.repository.MemberRepository;
 import com.jsm.mm.domain.memberlocation.MemberLocation;
 import com.jsm.mm.domain.memberlocation.repository.MemberLocationRepository;
@@ -24,7 +25,9 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -101,5 +104,40 @@ class MemberApiControllerTest {
         assertThat(memberLocation.getId().getLocation().getId()).isEqualTo(signUpRequestDto.getLocation());
 
         assertThat(memberRangeLocationSize).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("계정 존재 여부 확인")
+    void isExistMember() throws Exception {
+        // given
+        String column1 = "username";
+        String value1 = "test123";
+        String column2 = "email";
+        String value2 = "test1234@naver.com";
+
+        memberRepository.save(Member.builder()
+                .username("test123")
+                .password("~test123")
+                .email("test123@naver.com")
+                .nickname("test123")
+                .isLeave(false)
+                .roleName(RoleName.GUEST)
+                .build());
+
+        String url1 = String.format("/api/member/exist/%s?v=%s", column1, value1);
+        String url2 = String.format("/api/member/exist/%s?v=%s", column2, value2);
+
+        // when
+        ResultActions actions1 = mvc.perform(get(url1));
+        ResultActions actions2 = mvc.perform(get(url2));
+
+        // then
+        actions1
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"));
+
+        actions2
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
     }
 }
