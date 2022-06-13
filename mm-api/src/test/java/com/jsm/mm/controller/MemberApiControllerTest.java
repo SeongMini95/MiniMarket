e5b33d212bc344243c850dbd5785a7611c7b9fe7
SeongMini95 <modify879@gmail.com>
2022-06-13 20:14:46 +0900

    test: 회원가입 이메일 인증 보내기 추가

--- mm-api/src/test/java/com/jsm/mm/controller/MemberApiControllerTest.java
+++ mm-api/src/test/java/com/jsm/mm/controller/MemberApiControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("not-signUpEmail")
 class MemberApiControllerTest {
 
     @Autowired
